26364aab5357678c45bb279564f5934345a9ed0b
Mukkund Sunjii <max.suclatter@gmail.com>
2022-09-05 22:56:12 +0200

    Update build_script.sh
    added echo statements

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,11 +1,15 @@
+echo "Installing Rustup..."
 # Install Rustup (compiler)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+echo "Installing wasm-pack..."
 # Install wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
+echo "Building wasm-parser..."
 # Build wasm-parser 
 npm run build:wasm
 
+echo "Build static frontend client..."
 # Build static html for the react client
 npm run build
