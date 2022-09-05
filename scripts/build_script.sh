f49ad7cf6e5ce58c66db20de1b38defb56a37c42
Mukkund Sunjii <max.suclatter@gmail.com>
2022-09-05 23:18:03 +0200

    Update build_script.sh
    modfied build script to add binaries to path

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,6 +1,7 @@
 echo "Installing Rustup..."
 # Install Rustup (compiler)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
 
 echo "Installing wasm-pack..."
 # Install wasm-pack
