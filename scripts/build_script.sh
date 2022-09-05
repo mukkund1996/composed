2d575bbb2eee0ba6964db44bce48b70ef0d0228c
Mukkund Sunjii <max.suclatter@gmail.com>
2022-09-05 22:38:19 +0200

    Create build_script.sh
    Added build scripts

---
+++ scripts/build_script.sh
@@ -0,0 +1,11 @@
+# Install Rustup (compiler)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install wasm-pack
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
+# Build wasm-parser 
+npm run build:wasm
+
+# Build static html for the react client
+npm run build
